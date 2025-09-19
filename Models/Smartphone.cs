29b793ba15e7647f2a6f591b51a9a357bb15ceb7
Pitonico <dio_dgba@hotmail.com>
2025-09-19 16:05:03 -0300

    alterações feitas

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,15 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public required string Marca { get; init; }
+        public required string Modelo { get; init; }
+        public required string Imei { get; init; }
+        public required int Memoria { get; init; }
+        public required int Armazenamento { get; init; }
 
         public Smartphone(string numero)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
@@ -22,5 +25,15 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+        
+        public override string ToString()
+        {
+            return $"Marca: {Marca} - " +
+                $"Modelo: {Modelo} - " +
+                $"IMEI: {Imei} - " +
+                $"Memória: {Memoria}GB - " +
+                $"Armazenamento: {Armazenamento}GB - " +
+                $"Número: {Numero}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456789") {
+            Marca = "Nokia",
+            Modelo = "3310",
+            Imei = "987654321098765",
+            Memoria = 1,
+            Armazenamento = 16
+        };
+
+        Console.WriteLine("---| " + nokia.ToString() + " |---");
+      
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Smartphone iphone = new Iphone("987654321") {
+            Marca = "Apple",
+            Modelo = "iPhone 13",
+            Imei = "123456789012345",
+            Memoria = 4,
+            Armazenamento = 128
+        };
+
+        Console.WriteLine("---| " + iphone.ToString() + " |---");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Facebook");
+    }
+}
